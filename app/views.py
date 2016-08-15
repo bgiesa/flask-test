3271d87458dc7b4d22d8c6f4a4d104b67d5bd1ae
Bastian Giesa <bgiesa@gwdg.de>
2016-08-15 08:13:49 +0200

    login fix

--- app/views.py
+++ app/views.py
@@ -13,20 +13,13 @@ def load_user(user_id):
 
 @app.route('/')
 @app.route('/index')
-def show_entries():
-    # db = get_db()
-    # cur = db.execute('select title, text from entries order by id desc')
-    # entries = cur.fetchall()
-    # return render_template('show_entries.html', entries=entries)
+def show_entries():    
     return render_template('layout.html')
 
 @app.route('/add', methods=['POST'])
 def add_entry():
     if not session.get('logged_in'):
-        abort(401)
-    # db = get_db()
-    # db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
-    # db.commit()
+        abort(401)    
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
@@ -36,17 +29,7 @@ def show_user():
     return (render_template('admin/show_user.html',users=users))
 
 @app.route('/login', methods=['GET', 'POST'])
-def login():
-    # error = None
-    # if request.method == 'POST':
-    #     if request.form['username'] != app.config['USERNAME']:
-    #         error = 'Invalid username'
-    #     elif request.form['password'] != app.config['PASSWORD']:
-    #         error = 'Invalid password'
-    #     else:
-    #         session['logged_in'] = True
-    #         flash('You were logged in')
-    #         return redirect(url_for('show_entries'))
+def login():    
     error = None
     form = LoginForm()
     
@@ -58,7 +41,8 @@ def login():
             error = 'Invalid Password'
         else:
             login_user(user)
-            flash('You were logged in')
+            session['logged_in'] = True
+            flash('You were logged in')            
             return redirect(url_for('show_entries'))
 
     return render_template('login.html', error=error, form=form)
@@ -66,6 +50,6 @@ def login():
 @app.route('/logout')
 def logout():
     logout_user()
-    # session.pop('logged_in', None)
+    session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
